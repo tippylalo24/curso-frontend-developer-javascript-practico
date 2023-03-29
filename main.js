1f865767677a03f8392d38efc41befc0498c3fc5
Eduardo Lopez <jlopezd498@hotmail.com>
2023-03-29 15:51:24 -0600

    Se agregó la lista de productos.

--- main.js
+++ main.js
@@ -51,3 +51,82 @@ function toggleShoppingMenu(){
 
     productDetail.classList.toggle("inactive");
 }
+
+
+//Productos en el container
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productsList.push({
+    name: 'Skateboard',
+    price: 80,
+    image: 'https://images.pexels.com/photos/2734777/pexels-photo-2734777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productsList.push({
+    name: 'Wheel',
+    price: 60,
+    image: 'https://images.pexels.com/photos/161921/old-wheel-wheel-cart-old-161921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productsList.push({
+    name: 'Car',
+    price: 120,
+    image: 'https://images.pexels.com/photos/457418/pexels-photo-457418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productsList.push({
+    name: 'Flight',
+    price: 80,
+    image: 'https://images.pexels.com/photos/358319/pexels-photo-358319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productsList.push({
+    name: 'Train',
+    price: 60,
+    image: 'https://images.pexels.com/photos/72594/japan-train-railroad-railway-72594.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+
+const cardsContainer = document.querySelector(".cards-container")
+
+/* Esta función imrpime los productos que estén dentro del arreglo */
+function renderProducts(arrayOfProducts){
+    for(product of arrayOfProducts){
+        /*<div class="product-card">*/
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        /*<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""> */
+        const imgProduct = document.createElement('img');
+        imgProduct.setAttribute('src', product.image)
+        /* <div class="product-info"> */
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        /*<div>*/
+        const productInfoDiv = document.createElement('div');
+        /*<p>$120,00</p>*/
+        const productInfoDivPrice = document.createElement('p');
+        productInfoDivPrice.innerText = '$' + product.price;
+        /*<p>Bike</p>*/
+        const productInfoDivName = document.createElement('p');
+        productInfoDivName.innerText = product.name;
+
+        productInfoDiv.appendChild(productInfoDivPrice);
+        productInfoDiv.appendChild(productInfoDivName);
+        /* <figure> */
+        const productInfoFigure = document.createElement('figure');
+        /* <img src="./icons/bt_add_to_cart.svg" alt=""> */
+        const productInfoFigureImg = document.createElement('img');
+        productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg')
+        
+        productInfoFigure.appendChild(productInfoFigureImg);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure)
+
+        productCard.appendChild(imgProduct);
+        productCard.appendChild(productInfo);
+        
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productsList);
