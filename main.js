4d443d8dfffb21e9f75321b51ff17d6172e0937f
Eduardo Lopez <jlopezd498@hotmail.com>
2023-03-28 22:10:25 -0600

    Se agregó el carrito de compras y funcionalidad cuando se da clic en -carrito de compras-, -menú mobile- y -menu desktop-

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
+//Funcionalidad para el botón "navbar-email"
 const menuEmail = document.querySelector(".navbar-email");
-
 const desktopMenu = document.querySelector(".desktop-menu");
-
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    const isProductDetailMenuOpen = !productDetail.classList.contains("inactive");
+
+    if(isProductDetailMenuOpen){
+        productDetail.classList.toggle("inactive");
+    }
+    
     desktopMenu.classList.toggle("inactive");
 }
-console.log(":v")
+
+//Funcionalidad para el botón "menu-icon"
+const  mobileMenu = document.querySelector(".mobile-menu");
+const  menuIcon = document.querySelector(".menu");
+
+menuIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    const isProductDetailMenuOpen = !productDetail.classList.contains("inactive");
+
+    if(isProductDetailMenuOpen){
+        productDetail.classList.toggle("inactive");
+    }
+    
+    mobileMenu.classList.toggle("inactive");
+}
+
+//Funcionalidad para el botón "carrito de compras"
+const  productDetail = document.querySelector(".product-detail");
+const  shoppingCart = document.querySelector(".navbar-shopping-cart");
+
+shoppingCart.addEventListener('click', toggleShoppingMenu);
+
+function toggleShoppingMenu(){
+    const isDesktopMenuMenuOpen = !desktopMenu.classList.contains("inactive");
+
+    if(isDesktopMenuMenuOpen){
+        desktopMenu.classList.toggle("inactive");
+    }
+
+    const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+
+    if(isMobileMenuOpen){
+        mobileMenu.classList.toggle("inactive");
+    }
+
+    productDetail.classList.toggle("inactive");
+}
